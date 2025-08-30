948b21e6660e48b62b2e730ddd047f1d59a9a95f
Pratham <patadiyapratham98@gmail.com>
2025-08-30 19:15:27 +0530

    Keep my version of code, resolved conflicts

--- hackout25/hackout25/urls.py
+++ hackout25/hackout25/urls.py
@@ -22,9 +22,14 @@ from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+<<<<<<< Updated upstream
     path("dashboard/", include("dashboard.urls")),
     path('news/', include('news.urls')),
     path('', include('authentication.urls')),
+=======
+    path('', include('dashboard.urls')),
+    path('news/', include('news.urls')),
+>>>>>>> Stashed changes
 ]
 
 # Serve static files during development
